9290637f8dfd24f3fa8439f695bc83e9a787b54e
REMY-2401001541 <johnthanmugabe@gmail.com>
2025-04-12 19:34:36 +0200

    Add files via upload

---
+++ get.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Form (GET Method)</title>
+</head>
+<body>
+
+<h2>Enter Your Password</h2>
+
+<form method="get" action="">
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password">
+    <input type="submit" value="Submit">
+</form>
+
+<?php
+// Check if password is submitted
+if (isset($_GET['password'])) {
+    $password = $_GET['password'];
+    echo "<p>You entered: <strong>$password</strong></p>";
+}
+?>
+
+</body>
+</html>
