6dcfbb04e6bf5975940cb89e0be5929585e66551
REMY-2401001541 <johnthanmugabe@gmail.com>
2025-04-12 18:15:04 +0200

    Add files via upload

---
+++ .php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+</head>
+<body>
+    echo "Hello World!";
+    <br>
+    echo "are you fine!";
+    <br>
+    echo "I am fine!";
+    <br>
+    echo "I am fine too!";
+    <br>
+    echo "I am fine too!";
+    <br>
+    echo "I am fine too!";
+
+</body>
+</html>
