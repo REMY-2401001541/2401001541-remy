0050e16dbdac167a0d2b56e68bc08556cb93ee5c
REMY-2401001541 <johnthanmugabe@gmail.com>
2025-04-12 18:40:04 +0200

    Add files via upload

---
+++ numbers.php
@@ -0,0 +1,25 @@
+<?php
+// 1. Summation of numbers
+$number1 = 10;
+$number2 = 20;
+$sum = $number1 + $number2;
+
+echo "Sum: $sum<br>"; // Output: Sum: 30
+
+// 2. Increment
+$sum++; // Same as $sum = $sum + 1;
+echo "After Increment: $sum<br>"; // Output: 31
+
+// 3. Decrement
+$sum--; // Same as $sum = $sum - 1;
+echo "After Decrement: $sum<br>"; // Output: 30
+
+// 4. Finding the minimum value
+$a = 5;
+$b = 12;
+$c = 3;
+
+$min = min($a, $b, $c);
+
+echo "Minimum value is: $min"; // Output: Minimum value is: 3
+?>
